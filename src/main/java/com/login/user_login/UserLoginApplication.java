5b7373ee1a3850dadaa1c5bb243bd79edd26c56a
kai <1363560774@qq.com>
2023-07-21 11:53:49 +0800

    mybatis-plus 无法使用的问题

--- src/main/java/com/login/user_login/UserLoginApplication.java
+++ src/main/java/com/login/user_login/UserLoginApplication.java
@@ -1,9 +1,11 @@
 package com.login.user_login;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.login.user_login.mapper")
 public class UserLoginApplication {
 
     public static void main(String[] args) {
