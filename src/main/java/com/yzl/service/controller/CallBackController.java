d4900c78f7961a294f130e9f098b72bd4c4e8399
kai <1363560774@qq.com>
2023-10-26 10:15:22 +0800

    腾讯语音文件识别

--- src/main/java/com/yzl/service/controller/CallBackController.java
+++ src/main/java/com/yzl/service/controller/CallBackController.java
@@ -4,7 +4,7 @@ import com.yzl.service.common.ReturnMessage;
 import com.yzl.service.service.CallBackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class CallBackController {
     @Autowired
     private CallBackService callBackService;
 
-    @GetMapping("/describeTaskResponse")
+    @PostMapping("/describeTaskResponse")
     public ResponseEntity<Object> describeTaskResponse(Integer code, String message, Long requestId, Long appid, Long projectid, String audioUrl, String text, String resultDetail, Double audioTime){
         return ResponseEntity.ok().body(ReturnMessage.successMessage(callBackService.describeTaskResponse()));
     }
