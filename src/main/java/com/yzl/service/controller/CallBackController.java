5ff6ed1bc04f4772dbe9d017aa0a29af6ab2d218
kai <1363560774@qq.com>
2023-10-26 10:06:57 +0800

    腾讯语音文件识别

---
+++ src/main/java/com/yzl/service/controller/CallBackController.java
@@ -0,0 +1,22 @@
+package com.yzl.service.controller;
+
+import com.yzl.service.common.ReturnMessage;
+import com.yzl.service.service.CallBackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("callBack")
+public class CallBackController {
+
+    @Autowired
+    private CallBackService callBackService;
+
+    @GetMapping("/describeTaskResponse")
+    public ResponseEntity<Object> describeTaskResponse(Integer code, String message, Long requestId, Long appid, Long projectid, String audioUrl, String text, String resultDetail, Double audioTime){
+        return ResponseEntity.ok().body(ReturnMessage.successMessage(callBackService.describeTaskResponse()));
+    }
+}
---
+++ src/main/java/com/yzl/service/service/impl/CallBackServiceImpl.java
@@ -0,0 +1,19 @@
+package com.yzl.service.service.impl;
+
+import com.yzl.service.service.CallBackService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author kai
+ * @date 2023/10/25 6:48 下午
+ */
+@Slf4j
+@Service
+public class CallBackServiceImpl implements CallBackService {
+
+    @Override
+    public String describeTaskResponse() {
+        return null;
+    }
+}
